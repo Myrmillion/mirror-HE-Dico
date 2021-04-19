ec40d8dbe89a530da31a418e58c2f0d5d4c923ef
Digier Guillaume <guillaume.digier@he-arc.ch>
2021-04-19 15:29:48 +0200

    Update HeDictionaryApplicationTests.java

--- src/test/java/ch/hearc/HeDictionaryApplicationTests.java
+++ src/test/java/ch/hearc/HeDictionaryApplicationTests.java
@@ -3,6 +3,7 @@ package ch.hearc;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 @WebMvcTest
 @SpringBootTest
